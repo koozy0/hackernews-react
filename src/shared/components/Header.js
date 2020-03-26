b1a8e274c630b7cadb0206b19e2af3c543428496
Koo Zheng Yu <zykoo@palo-it.com>
2020-03-26 14:43:59 +0800

    Add scss, linting and formatting configs, add basic components and directories

---
+++ src/shared/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <h1>navbar</h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
